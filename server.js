2b424bfbcfb0c97c86e085bf858a7332fe110b82
jegelber <gelber_j1@denison.edu>
2020-08-10 18:25:13 -0400

    test API endpoints

---
+++ server.js
@@ -0,0 +1,85 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongodb = require("mongodb");
+var ObjectID = mongodb.ObjectID;
+
+var ALGORITHMS_COLLECTION = "algorithms";
+
+var app = express();
+app.use(bodyParser.json());
+
+// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+var db;
+
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  // Save database object from the callback for reuse.
+  db = client.db();
+  console.log("Database connection ready");
+
+  // Initialize the app.
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
+});
+
+// ALGORITHMS API ROUTES BELOW
+
+// Generic error handler used by all endpoints.
+function handleError(res, reason, message, code) {
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({"error": message});
+}
+
+/*  "/api/algorithms"
+ *    GET: finds all algorithms
+ *    POST: creates a new algorithm
+ */
+
+ app.get("/api/algorithms", function(req, res) {
+   db.collection(ALGORITHMS_COLLECTION).find({}).toArray(function(err, docs) {
+     if (err) {
+       handleError(res, err.message, "Failed to get algorithms.");
+     } else {
+       res.status(200).json(docs);
+     }
+   });
+ });
+
+ app.post("/api/algorithms", function(req, res) {
+   var newAlgorithm = req.body;
+   newAlgorithm.createDate = new Date();
+
+   if (!req.body.name) {
+     handleError(res, "Invalid user input", "Must provide a name.", 400);
+   } else {
+     db.collection(ALGORITHMS_COLLECTION).insertOne(newAlgorithm, function(err, doc) {
+       if (err) {
+         handleError(res, err.message, "Failed to create new algorithm.");
+       } else {
+         res.status(201).json(doc.ops[0]);
+       }
+     });
+   }
+ });
+
+/*  "/api/algorithms/:id"
+ *    GET: find algorithm by id
+ *    PUT: update algorithm by id
+ *    DELETE: deletes algorithm by id
+ */
+
+app.get("/api/algorithms/:id", function(req, res) {
+});
+
+app.put("/api/algorithms/:id", function(req, res) {
+});
+
+app.delete("/api/algorithms/:id", function(req, res) {
+});
