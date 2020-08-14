c460687724320c8ffcde46adf6dc818775c86e5e
jegelber <gelber_j1@denison.edu>
2020-08-14 15:05:11 -0400

    updated home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AlgorithmService } from '../algorithms/algorithm.service';
+import { Algorithm } from '../algorithms/algorithm';
+
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent implements OnInit {
+  algorithms: Algorithm[];
+  favoriteAlgs: Algorithm[];
+  randomAlgorithm: Algorithm;
 
-  constructor() { }
+  constructor(
+    private algorithmService: AlgorithmService
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.getAlgorithms();
   }
 
+  getAlgorithms(): void {
+    this.algorithmService.getAlgorithms()
+    .subscribe(algorithms => {
+      this.algorithms = algorithms;
+      this.favoriteAlgs = algorithms.slice(0, 4);
+    });
+  }
+
+  random(): void {
+    let random = Math.floor(Math.random() * this.algorithms.length);
+    this.randomAlgorithm = this.algorithms[random];
+  }
 }
