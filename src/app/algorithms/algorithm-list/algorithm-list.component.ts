84b932becb00d43c65e4b263f998aea398f61f0c
jegelber <gelber_j1@denison.edu>
2020-08-14 14:15:15 -0400

    updated algorithm-list component

--- src/app/algorithms/algorithm-list/algorithm-list.component.ts
+++ src/app/algorithms/algorithm-list/algorithm-list.component.ts
@@ -8,9 +8,15 @@ import { Algorithm } from '../algorithm';
   templateUrl: './algorithm-list.component.html',
   styleUrls: ['./algorithm-list.component.css']
 })
-export class AlgorithmListComponent implements OnInit {
+export class AlgorithmsComponent implements OnInit {
+
+  groups = new Set<string>();
 
   algorithms: Algorithm[];
+  filteredAlgorithms: Algorithm[];
+  selectedAlg: Algorithm;
+  selectedGroup: string;
+  filtered: boolean = false;
 
   constructor(
     private algorithmService: AlgorithmService
@@ -24,7 +30,27 @@ export class AlgorithmListComponent implements OnInit {
     this.algorithmService.getAlgorithms()
       .subscribe(algorithms => {
         this.algorithms = algorithms;
+        this.getGroups();
       });
+  }
+
+  getGroups(): void {
+    for (const key of Object.keys(this.algorithms)) {
+      const value = this.algorithms[key];
+      this.groups.add(value.group);
+    };
+  }
+
+  filterAlgorithms(group: string): void {
+    if (group === "all") {
+      this.filtered = false;
+    } else {
+      this.algorithmService.filterAlgorithms(group)
+        .subscribe(algorithms => {
+          this.filteredAlgorithms = algorithms;
+          this.filtered = true;
+        });
+    }
   }
 
 }
