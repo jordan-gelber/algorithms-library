3899feff08f407c7e98530eb13f89a09ae476b19
jegelber <gelber_j1@denison.edu>
2020-08-14 12:56:19 -0400

    update project

--- server.js
+++ server.js
@@ -75,11 +75,36 @@ function handleError(res, reason, message, code) {
  *    DELETE: deletes algorithm by id
  */
 
-app.get("/api/algorithms/:id", function(req, res) {
-});
+ app.get("/api/algorithms/:id", function(req, res) {
+   db.collection(ALGORITHMS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
+     if (err) {
+       handleError(res, err.message, "Failed to get algorithm");
+     } else {
+       res.status(200).json(doc);
+     }
+   });
+ });
 
-app.put("/api/algorithms/:id", function(req, res) {
-});
+ app.put("/api/algorithms/:id", function(req, res) {
+   var updateDoc = req.body;
+   delete updateDoc._id;
 
-app.delete("/api/algorithms/:id", function(req, res) {
-});
+   db.collection(ALGORITHMS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
+     if (err) {
+       handleError(res, err.message, "Failed to update algorithm");
+     } else {
+       updateDoc._id = req.params.id;
+       res.status(200).json(updateDoc);
+     }
+   });
+ });
+
+ app.delete("/api/algorithms/:id", function(req, res) {
+   db.collection(ALGORITHMS_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
+     if (err) {
+       handleError(res, err.message, "Failed to delete algorithm");
+     } else {
+       res.status(200).json(req.params.id);
+     }
+   });
+ });
