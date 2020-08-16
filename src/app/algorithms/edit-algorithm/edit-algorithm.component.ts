944b8b518318b474b0ad9ed4ce4173633b4806c1
jegelber <gelber_j1@denison.edu>
2020-08-16 13:06:26 -0400

    updated edit algorithm component

--- src/app/algorithms/edit-algorithm/edit-algorithm.component.ts
+++ src/app/algorithms/edit-algorithm/edit-algorithm.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { AlgorithmService } from '../algorithm.service';
+import { Algorithm } from '../algorithm';
 
 @Component({
   selector: 'app-edit-algorithm',
@@ -6,10 +11,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-algorithm.component.css']
 })
 export class EditAlgorithmComponent implements OnInit {
+  algorithm: Algorithm;
+  algorithms: Algorithm[];
+  editor: boolean = false;
+  editSubVisible: boolean = false;
+  createSubVisible: boolean = false;
+  selectedSub: string;
 
-  constructor() { }
+  constructor(
+    private location: Location,
+    private route: ActivatedRoute,
+    private router: Router,
+    private algorithmService: AlgorithmService
+  ) { }
 
   ngOnInit(): void {
+    this.getAlgorithms();
+    this.route.params.subscribe(routeParams => {
+    	this.getAlgorithm(routeParams.name);
+    });
+  }
+
+  getAlgorithm(name: string): void {
+    this.algorithmService.getAlgorithmByName(name)
+      .subscribe(algorithm => this.algorithm = algorithm);
+  }
+
+  getAlgorithms(): void {
+    this.algorithmService.getAlgorithms()
+      .subscribe(algorithms => this.algorithms = algorithms);
+  }
+
+  back(): void {
+    this.location.back();
+  }
+
+  save(): void {
+    this.algorithmService.updateAlgorithm(this.algorithm)
+      .subscribe(() => {
+        this.router.navigateByUrl(`/algorithms/${this.algorithm.name}`);
+      });
+  }
+
+  editSubroutine(): void {
+    if (this.algorithm.subroutine) {
+      this.selectedSub = this.algorithm.subroutine.subname.concat(',');
+      this.selectedSub = this.selectedSub.concat(this.algorithm.subroutine.subtitle);
+    }
+    this.editSubVisible = true;
+  }
+
+  saveSubroutine(selectedSub: string): void {
+    this.editSubVisible = false;
+    let temp = selectedSub.split(",");
+    if (temp[0] !== "no") {
+      this.algorithm.subroutine = {subname: temp[0], subtitle: temp[1]};
+    } else {
+      this.algorithm.subroutine = null;
+    }
   }
 
 }
